481f485f55caea26024674e4b383533224b644ea
= <=>
2016-03-17 18:44:24 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  get    'verify'  => 'sessions#verify_access_token'
+  resources :users
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
+end
