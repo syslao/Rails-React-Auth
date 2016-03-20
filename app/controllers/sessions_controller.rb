368ad41e1858e8ec038ce60a20e30c5acbc92cc0
= <=>
2016-03-19 21:58:32 -0500

    improved views look, still on the work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -6,14 +7,18 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     respond_to do |format|
       if user && user.authenticate(params[:session][:password])
-        format.json { render :text => user.access_token }
-        # log_in user
-        # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        # redirect_back_or user
+        format.json { render :text => user.access_token, status: 201 }
+        format.html {
+          log_in user
+          params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+          redirect_back_or user
+        }
       else
         format.json { render text: "Email and password combination are invalid", status: :unprocessable_entity }
-        # flash.now[:danger] = 'Invalid email/password combination'
-        # render 'new'
+        format.html {
+          flash.now[:danger] = 'Invalid email/password combination'
+          render 'new'
+        }
       end
     end
   end
@@ -21,7 +26,7 @@ class SessionsController < ApplicationController
   def verify_access_token
     user = User.find_by(access_token: params[:session][:access_token])
     if user
-      render :text => "verified"
+      render text: "verified"
     else
       render text: "Token failed verification", status: :unprocessable_entity
     end
